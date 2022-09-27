3e47fbfc8fd370934fea7faf2373d0aa2660e2f1
Rachmizard <rachmizard11072000@gmail.com>
2022-09-27 17:22:16 +0700

    chore: react-query setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import type { AppProps } from "next/app";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import {
+	Hydrate,
+	DehydratedState,
+	QueryClientProvider,
+	QueryClient,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "../styles/globals.css";
 
 type MyAppProps = {
 	session?: Session;
+	dehydratedState?: DehydratedState;
 };
 
-function MyApp({ Component, pageProps }: AppProps<MyAppProps>) {
+function MyApp({
+	Component,
+	pageProps: { dehydratedState, session, ...pageProps },
+}: AppProps<MyAppProps>) {
+	const [queryClient] = useState(() => new QueryClient());
+
 	return (
-		<SessionProvider session={pageProps.session}>
-			<Component {...pageProps} />;
+		<SessionProvider session={session}>
+			<QueryClientProvider client={queryClient}>
+				<Hydrate state={dehydratedState}>
+					<Component {...pageProps} />;
+				</Hydrate>
+
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</SessionProvider>
 	);
 }
